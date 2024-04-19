b6abff171b375ebfb3fb5da0b305cada7a6546b5
unknown <Jacob-Barber@live.nmit.ac.nz>
2024-04-20 01:09:38 +1200

    have a semi functional grid and readline input

--- TicTacToe.js
+++ TicTacToe.js
@@ -31,7 +31,37 @@
 // gridx3 =[7,8,9]
 
 // console.log(gridx2[2])
-let sign = console.log("Player 1 turn");
+
+// let sign = console.log("Welcome to tictactoe");
+
+// const readline = require('readline')
+
+// const r1 = readline.Interface({
+//     input: process.stdin,
+//     outpu: process.stdout
+// })
+
+// r1.question('P1 move', (answer) => {
+//     console.log(answer.concat(' is your move'))
+//     process.exit()      
+// }) 
+
+// let x1 = [];
+// let x2 = [];
+// let x3 = [];
+
+// for (i=1; i<4; i++) {
+//     x1.push(i)
+// } console.log(x1)
+
+// for (i=4; i<7; i++) {
+//     x2.push(i)
+// } console.log(x2)
+
+// for (i=7; i<10; i++) {
+//     x3.push(i)
+// } console.log(x3)
+
 const readline = require('readline')
 
 const r1 = readline.Interface({
@@ -40,23 +70,28 @@ const r1 = readline.Interface({
 })
 
 
-
 let x1 = [];
 let x2 = [];
 let x3 = [];
 
-for (i=1; i<4; i++) {
-    x1.push(i)
-} console.log(x1)
-
-for (i=4; i<7; i++) {
-    x2.push(i)
-} console.log(x2)
-
-for (i=7; i<10; i++) {
-    x3.push(i)
-} console.log(x3)
-
-function p1move(p1rlinput) {
-
+function grid(){
+    for (i=1; i<4; i++) {
+        x1.push(i)
+    } console.log(x1)
+    for (i=4; i<7; i++) {
+        x2.push(i)
+    } console.log(x2)
+    for (i=7; i<10; i++) {
+        x3.push(i)
+    } console.log(x3)
 }
+
+grid()
+
+
+r1.question('Please input youer move ',(answer) => {    //if no text is included in question parameter the nit wont worrk for some reason.
+    console.log(answer.concat(" is your move"))
+    process.exit()
+})
+
+console.log(r1.question.answer)
