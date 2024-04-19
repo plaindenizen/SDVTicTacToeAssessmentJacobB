f1fdcfcf49f47175c31a54f623677ed26867a3d0
Jacob Barber <jakenbake12@live.com>
2024-04-19 15:37:18 +1200

    started work on grid

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,1 +1,12 @@
-pshh
+//SECTION - 3x3 grid using for loops
+
+let gridx = []
+let y = 0
+for (i=0; i<3; i++) {
+    
+    for (x=0; x<3; x++) {
+        y++
+        gridx.push(y)
+    }
+    console.log(gridx)
+}
